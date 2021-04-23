c0ae6e568fa4e1cb5bc87b38823a5c30a79caca4
MarcusHedenberg <82215349+MarcusHedenberg@users.noreply.github.com>
2021-04-23 14:18:18 +0200

    Lagt till ett ljud och la till mina knappar som prefabs

--- ProjectBox/Assets/Scripts/MusicController.cs
+++ ProjectBox/Assets/Scripts/MusicController.cs
@@ -7,10 +7,12 @@ public class MusicController : MonoBehaviour
 {
     [SerializeField] private AudioClip audioClip1;
     [SerializeField] private AudioClip audioClip2;
+    [SerializeField] private AudioClip deathSound;
     [SerializeField] private AudioSource audioSource;
 
     private bool firstTrack = true;
     private bool secondTrack = false;
+    private bool deathTrack = false;
 
 
     private void Start()
@@ -35,7 +37,14 @@ public class MusicController : MonoBehaviour
             audioSource.Play();
             secondTrack = false;
         }
- 
+        else if (deathTrack == true)
+        {
+            audioSource.clip = deathSound;
+            audioSource.loop = false;
+            audioSource.Play();
+            deathTrack = false;
+        }
+
     }
 
     public void ChangeClipWithButoon1()
@@ -46,5 +55,9 @@ public class MusicController : MonoBehaviour
     public void ChangeClipWithButton2()
     {
         secondTrack = true;
+    }
+    public void ChangeClipWithButton3()
+    {
+        deathTrack = true;
     }
 }
