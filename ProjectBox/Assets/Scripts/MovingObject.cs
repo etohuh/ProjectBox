d87a37041b1029a80749d8822e6ae122bf307d5f
baltzius <71239999+baltzius@users.noreply.github.com>
2021-05-01 16:09:21 +0200

    Lagt till flyttande plattformar, killzon på laserzon

---
+++ ProjectBox/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public GameObject point1;
+    public GameObject point2;
+    private GameObject nextTarget;
+    [SerializeField] private float speed = 1f;
+    void Start()
+    {
+        nextTarget = point2;
+    }
+    void Update()
+    {
+        MoveToPosition(nextTarget);
+    }
+    void MoveToPosition(GameObject moveToTarget)
+    {
+        gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, moveToTarget.transform.position, speed * Time.deltaTime);
+        if (gameObject.transform.position == moveToTarget.transform.position)
+        {
+            ChangeTarget();
+        }
+    }
+    private void ChangeTarget()
+    {
+        if (nextTarget == point2)
+        {
+            nextTarget = point1;
+        }
+        else
+        {
+            nextTarget = point2;
+        }
+    }
+}
