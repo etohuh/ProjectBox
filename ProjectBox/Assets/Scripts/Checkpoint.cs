42f1180f4bdd41fb791b0c91d254f4114a03f3c2
etohuh <gregdiamondd@gmail.com>
2021-05-28 17:19:56 +0200

    kok

--- ProjectBox/Assets/Scripts/Checkpoint.cs
+++ ProjectBox/Assets/Scripts/Checkpoint.cs
@@ -9,10 +9,13 @@ public class Checkpoint : MonoBehaviour
     // Start is called before the first frame update
     private SpriteRenderer _sRenderer;
     private Transform _transform;
+
+    public AudioSource checkpointSound;
     
     private bool doMovement;
     private Vector3 target;
     public GameObject text;
+    public bool checkPointTaken;
     void Start()
     {
         
@@ -20,7 +23,7 @@ public class Checkpoint : MonoBehaviour
         _transform = gameObject.GetComponent<Transform>();
         
         target = new Vector3(_transform.position.x + 0.8f, _transform.position.y, 0f);
-        
+        checkPointTaken = false;
         text.SetActive(false);
     }
 
@@ -36,10 +39,15 @@ public class Checkpoint : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<PlayerState>().SetRespawnPos(gameObject.transform);
-            doMovement = true;
-            _sRenderer.color = Color.white;
-            text.SetActive(true);
+            if (!checkPointTaken)
+            {
+                checkpointSound.Play();
+                collision.gameObject.GetComponent<PlayerState>().SetRespawnPos(gameObject.transform);
+                doMovement = true;
+                _sRenderer.color = Color.white;
+                text.SetActive(true);
+                checkPointTaken = true;
+            }
 
         } 
         
