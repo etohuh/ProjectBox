262537b95c87b5542f1acd121bc84a6f6d1fc616
baltzius <71239999+baltzius@users.noreply.github.com>
2021-05-29 15:45:11 +0200

    okidoki

---
+++ ProjectBox/Assets/Scripts/AudioOneShot.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioOneShot : MonoBehaviour
+{
+
+    public AudioClip impact;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter()
+    {
+        audioSource.PlayOneShot(impact, 0.7F);
+    }
+}
